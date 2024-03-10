61ee6f116908fb5653b4f7860730d8c379d93a7e
AnaJCastro <105999305+AnaJCastro@users.noreply.github.com>
2024-03-10 11:59:25 -0300

    Meu conversor de real-dolar e de dolar-real

---
+++ conversao-dolar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    float dolar, real, conversao;
+    dolar = 4.98; //valor atual do dolar (03/2024)
+    real = 0.20; //valor atual do real (03/2024)
+
+    printf("insira qual conversao voce deseja: insira 1 para real-dolar ou insira 2 para dolar-real");
+    int tipo;
+    tipo = 0;
+    scanf("%i",&tipo);
+    switch (tipo)
+    {
+    case 1:
+         printf("insira o valor em dolar");
+        scanf("%f",&real);
+        conversao = dolar*real;
+        printf("a sua conversao sera %.2f",conversao);
+        break;
+        
+    case 2:
+         printf("insira o valor em real");
+         scanf("%f",&dolar);
+        conversao = real*dolar;
+        printf("a sua conversao sera %.2f",conversao);
+        break;
+
+    default:
+        printf("Essa opção não existe!");
+        break;
+    }
+}
